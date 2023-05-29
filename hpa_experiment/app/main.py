f39466a8f461fa3d7d38c3b3552d977d76d44ef9
NeroHin <hin1997101@gmail.com>
2023-05-29 23:55:34 +0800

    feat: complete the api demo and loading test setup

--- hpa_experiment/app/main.py
+++ hpa_experiment/app/main.py
@@ -6,7 +6,10 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    time.sleep(15) # Sleep for 15 seconds
     host_name = socket.gethostname() 
     host_ip = socket.gethostbyname(host_name) 
-    return {"IP": host_ip, "Port": 8000}
+    return {
+        "Message": "You bought the ticket",
+        "IP": host_ip, 
+        "Port": 8000
+        }
---
+++ hpa_experiment/locust_test.py
@@ -0,0 +1,8 @@
+from locust import HttpUser, task, between
+
+class FastAPIUser(HttpUser):
+    wait_time = between(1, 2.5)
+
+    @task
+    def get_root(self):
+        self.client.get("/")
